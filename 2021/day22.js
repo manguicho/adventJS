3add65646823d27dd6252f302917cd82a5f9d15f
Sebastian Mangisch <manguicho@gmail.com>
2021-12-22 14:05:21 -0300

    dia 21

---
+++ 2021/day22.js
@@ -0,0 +1,89 @@
+function countDecorations(bigTree) {
+ // ¡No olvides compartir tu solución en redes!
+ console.log(Object.entries(flattenObject(bigTree)).filter(e=>e[0].indexOf("value")!=-1).map(e=>e[1]).reduce((a,b)=>a+b))
+  return 0
+}
+
+function flattenObject(ob) {
+    var toReturn = {};
+    for (var i in ob) {
+        if (!ob.hasOwnProperty(i)) continue;
+        if ((typeof ob[i]) == 'object' && ob[i] !== null) {
+            var flatObject = flattenObject(ob[i]);
+            for (var x in flatObject) {
+                if (!flatObject.hasOwnProperty(x)) continue;
+                toReturn[i + '.' + x] = flatObject[x];
+            }
+        } else {
+            toReturn[i] = ob[i];
+        }
+    }
+    return toReturn;
+}
+
+// tenemos el árbol en forma de objeto
+const tree = {
+  value: 1, // el nodo raíz siempre es uno, porque es la estrella ⭐
+  left: {
+    value: 2, // el nodo izquierdo necesita dos decoraciones
+    left: null, // no tiene más ramas
+    right: null // no tiene más ramas
+  },
+  right: {
+    value: 3, // el nodo de la derecha necesita tres decoraciones
+    left: null, // no tiene más ramas
+    right: null // no tiene más ramas
+  }
+}
+
+/* Gráficamente sería así:
+    1
+  /   \
+ 2     3
+
+1 + 2 + 3 = 5
+*/
+
+countDecorations(tree) // 5
+
+const bigTree = {
+  value: 1,
+  left: {
+    value: 5,
+    left: {
+      value: 7,
+      left: {
+        value: 3,
+        left: null,
+        right: null
+      },
+      right: null
+    },
+    right: null
+  },
+  right: {
+    value: 6,
+    left: {
+      value: 5,
+      left: null,
+      right: null
+    },
+    right: {
+      value: 1,
+      left: null,
+      right: null
+    }
+  }
+}
+
+/*
+        1
+      /   \
+     5     6
+    /     / \
+   7     5   1
+  /
+ 3
+*/
+
+countDecorations(bigTree) // 28
