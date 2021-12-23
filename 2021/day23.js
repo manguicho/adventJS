f9fa2cc7bf8bde403433116bad41e77c7d0afd3b
Sebastian Mangisch <manguicho@gmail.com>
2021-12-23 14:20:51 -0300

    dia 23 v2

--- 2021/day23.js
+++ 2021/day23.js
@@ -2,7 +2,9 @@ function canReconfigure(from, to) {
   if(from.length != to.length) return false  
   let buff = []
   for (let i in from.split("")){
-    if (!buff.map(e=>e.join("")).includes(from[i]+to[i]) && from[i]!=to[i] && (buff.map(e=>e[0]).includes(from[i]) || buff.map(e=>e[1]).includes(to[i]))) {console.log(i,buff, [from[i],to[i]]);return false}
+    if (!buff.map(e=>e.join("")).includes(from[i]+to[i]) && //si ya sucedio la asignacion es correcto entonces se omite en la comprobacion para false
+        from[i]!=to[i] && //puede asignarse a si misma
+        (buff.map(e=>e[0]).includes(from[i]) || buff.map(e=>e[1]).includes(to[i]))) return false //evitado lo anterior, si from o to ya fueron asignadas: false
     buff.push([from[i],to[i]])
   }
   return true
@@ -11,21 +13,21 @@ function canReconfigure(from, to) {
 let from = 'BAL'
 let to   = 'LIB'
 
- from = 'CON'
- to   = 'JUU'
- from = 'xxxMMM'
- to   = 'aaaDDD'
- from = 'abcdefghijkl'
- to = 'lkjihgfedcba'
- from = 'xxxqqqeeefffggg'
- to = 'xxxqqqeeefffgg'
+from = 'CON'
+to   = 'JUU'
+from = 'xxxMMM'
+to   = 'aaaDDD'
+from = 'abcdefghijkl'
+to = 'lkjihgfedcba'
+from = 'xxxqqqeeefffggg'
+to = 'xxxqqqeeefffgg'
 from = 'MMM'
 to = 'MID'
 
 from = 'abcdefghijkl'
 to = 'lkjihgfedcba'
 
- from = 'xxxMMM'
- to   = 'aaaDDD'
- console.log(canReconfigure(from, to))
+from = 'xxxMMM'
+to   = 'aaaDDD'
+console.log(canReconfigure(from, to))
  
