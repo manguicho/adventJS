b9246dbef82b4cc4525a28471a0bcdb00a702f6c
Sebastian Mangisch <manguicho@gmail.com>
2021-12-24 16:33:16 -0300

    dia 24

---
+++ 2021/day24.js
@@ -0,0 +1,39 @@
+ function checkIsSameTree(treeA, treeB) {
+ // ¡No olvides compartir tu solución en redes!
+ return Object.entries(flattenObject(treeA)).sort().toString() == Object.entries(flattenObject(treeB)).sort().toString()
+}
+
+function flattenObject(ob) {
+    var toReturn = {};
+    for (var i in ob) {
+        if (!ob.hasOwnProperty(i)) continue;
+        if ((typeof ob[i]) == 'object' && ob[i] !== null) {
+            var flatObject = flattenObject(ob[i]);
+            for (var x in flatObject) {
+                if (!flatObject.hasOwnProperty(x)) continue;
+                toReturn[i + '.' + x] = flatObject[x];
+            }
+        } else {
+            toReturn[i] = ob[i];
+        }
+    }
+    return toReturn;
+}
+
+
+const tree = {
+  value: 1,
+  left: { value: 2, left: null, right: null },
+  right: { value: 3, left: null, right: null }
+}
+
+checkIsSameTree(tree, tree) // true
+
+const tree2 = {
+  value: 1,
+  left: { value: 3, left: { value: 2, left: null, right: null }, right: null },
+  right: { value: 5, left: null, right: { value: 4, left: null, right: null } }
+}
+
+console.log(checkIsSameTree(tree, tree2)) // false
+console.log(checkIsSameTree(tree2, tree2)) // true mn mn
